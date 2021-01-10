fa6349b01244a4610de2f42e3d6ffad3befcfb44
Stacy Betts <stacyo23@yahoo.com>
2021-01-10 16:23:35 -0600

    updated orm with new functions

--- config/orm.js
+++ config/orm.js
@@ -41,7 +41,7 @@ function objToSql(ob) {
 
 // Object for all our SQL statement functions.
 var orm = {
-  all: function(tableInput, cb) {
+  Selectall: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
       if (err) {
@@ -50,7 +50,8 @@ var orm = {
       cb(result);
     });
   },
-  create: function(table, cols, vals, cb) {
+
+  insertOne: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -71,7 +72,7 @@ var orm = {
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
+  updateOne: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
@@ -80,6 +81,7 @@ var orm = {
     queryString += condition;
 
     console.log(queryString);
+
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
@@ -88,19 +90,7 @@ var orm = {
       cb(result);
     });
   },
-  delete: function(table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
-
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  }
+  
 };
 
 // Export the orm object for the model (burger.js).
