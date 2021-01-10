74d970e1e149af66125c4b0adb2ac2c9dc2e460d
Stacy Betts <stacyo23@yahoo.com>
2021-01-10 14:46:18 -0600

    added schema and seed data

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burger_db; 
+
+CREATE DATABASE burger_db; 
+
+USE burger_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT NOT NULL, 
+    burger_name VARCHAR(255) NOT NULL, 
+    devoured BOOLEAN NOT NULL, 
+    PRIMARY KEY(id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+Use burgers_db; 
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("CHICKEN BACON SWISS BURGER", FALSE); 
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("VEGGIE BURGER", FALSE); 
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("DOUBLE CHEESE BURGER", FALSE); 
